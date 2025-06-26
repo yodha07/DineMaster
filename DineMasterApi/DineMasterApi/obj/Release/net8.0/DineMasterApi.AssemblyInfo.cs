fc34a7c5bf67496f0989d3c0ae293079f20a35d2
AniketMGawade95 <aniketmgawade2019@gmail.com>
2025-06-26 21:24:00 +0530

    an

--- DineMasterApi/DineMasterApi/obj/Release/net8.0/DineMasterApi.AssemblyInfo.cs
+++ DineMasterApi/DineMasterApi/obj/Release/net8.0/DineMasterApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DineMasterApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30a6be28184ad2f33342f646645dcddbf0623bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DineMasterApi")]
